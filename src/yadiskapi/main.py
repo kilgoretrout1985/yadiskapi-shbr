43093f480345fdc37fd65c90195dd0e527128351
kilgoretrout1985 <zemskyura@gmail.com>
2022-09-11 11:04:32 +0300

    Вынес в отдельный блок код связанный с созданием sql-таблиц при запуске приложения.

--- src/yadiskapi/main.py
+++ src/yadiskapi/main.py
@@ -1,11 +1,8 @@
-import asyncio
 from typing import Any, Dict
 
 from fastapi import FastAPI, Depends
-from typer import Typer
 from sqlalchemy.orm import Session
 
-from yadiskapi.database import init_models
 from yadiskapi.routers import base, additional
 from yadiskapi.config import settings
 from yadiskapi.database import get_db
@@ -32,11 +29,25 @@ async def check_alive(db: Session = Depends(get_db)) -> Dict[str, Any]:
 
 
 if __name__ == "__main__":
+    import asyncio
+    from typer import Typer
+    from yadiskapi.database import init_models
+    # This is ugly, but I don't know how to fix this right now.
+    # If you do not import all models - it will not create corresponding 
+    # tables in the database.  
+    from yadiskapi.models import *
+    
+
     cli = Typer()
+
 
     @cli.command()
     def init_db(drop_all: bool = False):
+        """
+        cli command called directly `python main.py` to create db-tables at first run
+        """
         asyncio.run(init_models(drop_all))
         print("DB tables created.")
+
 
     cli()
